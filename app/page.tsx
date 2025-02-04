827fef2603a28a2c7165fa185add3ecc238aff21
Odyssey <77610388+Odyssey409@users.noreply.github.com>
2025-02-04 21:59:01 +0900

    #3.8 Lists and Animations

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,19 @@
 export default function Home() {
   return (
     <main className="flex items-center justify-center h-screen p-5 bg-gray-100 sm:bg-red-100 md:bg-green-100 lg:bg-cyan-100 xl:bg-orange-100 2xl:bg-purple-100 ">
-      <div className="flex flex-col w-full max-w-screen-sm gap-2 p-5 bg-white shadow-xl rounded-3xl md:flex-row  *:outline-none has-[.peer]:bg-green-100 ring ring-transparent transition-shadow has-[:invalid]:bg-red-100 has-[:invalid]:ring-red-500 has-[:invalid]:ring">
-        <input
-          type="text"
-          placeholder="Email address "
-          required
-          className="w-full h-10 py-3 pl-5 transition-shadow bg-gray-200 rounded-full ring ring-transparent focus:ring-green-500 focus:ring-offset-2 placeholder:drop-shadow invalid:focus:ring-red-500 peer "
-        />
-        <span className="hidden font-medium text-red-500 peer-invalid:block">
-          Email is required
-        </span>
-        <button className="py-2 font-medium text-white transition-transform rounded-full active:scale-90 md:px-10 bg-gradient-to-tr from-cyan-500 via-yellow-400 to-purple-400 peer-invalid:from-red-500">
-          Log in
-        </button>
+      <div className="flex flex-col w-full max-w-screen-sm gap-3 p-5 bg-white shadow-lg rounded-3xl ">
+        {["Nico", "Me", "You", "Yourself", ""].map((person, index) => (
+          <div key={index} className="flex items-center gap-5 ">
+            <div className="bg-blue-400 rounded-full size-10" />
+            <span className="text-lg font-medium empty:w-24 empty:h-5 empty:rounded-full empty:animate-pulse empty:bg-gray-300">
+              {person}
+            </span>
+            <div className="relative flex items-center justify-center text-white bg-red-500 rounded-full size-6">
+              <span className="z-10">{index}</span>
+              <div className="absolute bg-red-500 rounded-full size-6 animate-ping" />
+            </div>
+          </div>
+        ))}
       </div>
     </main>
   );
