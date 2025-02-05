20b37b90756308db8ce53b06e0ec030f8b9e2b59
Odyssey <77610388+Odyssey409@users.noreply.github.com>
2025-02-05 23:20:23 +0900

    #3.10 JIT
    tailwind.config.ts > theme > extend에 커스텀 class를 정의할 수 있음

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,7 +6,25 @@ const config: Config = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {},
+  theme: {
+    extend: {
+      borderRadius: {
+        "cool-name": "50px",
+      },
+      colors: {
+        "cool-blue": "#2196f3",
+        "cool-red": "#f44336",
+        "cool-green": "#4caf50",
+        "cool-yellow": "#ffeb3b",
+        "cool-pink": "#e91e63",
+        "cool-purple": "#9c27b0",
+        "cool-orange": "#ff9800",
+        "cool-brown": "#795548",
+        "cool-grey": "#9e9e9e",
+        "cool-blue-grey": "#607d8b",
+      },
+    },
+  },
   plugins: [],
 };
 export default config;
