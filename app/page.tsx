846a44e01f9d7de5f89f75d83283c1c214a481a2
Odyssey <77610388+Odyssey409@users.noreply.github.com>
2025-02-07 00:01:39 +0900

    #4.0 Home Screen

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,27 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <main className="flex items-center justify-center h-screen p-5 bg-gray-100 sm:bg-red-100 md:bg-green-100 lg:bg-cyan-100 xl:bg-orange-100 2xl:bg-purple-100 ">
-      <div className="flex flex-col w-full max-w-screen-sm gap-3 p-5 bg-white shadow-lg rounded-3xl ">
-        {["Nico", "Me", "You", "Yourself", ""].map((person, index) => (
-          <div key={index} className="flex items-center gap-5 group ">
-            <div className="bg-blue-400 rounded-full size-10" />
-            <span className="text-lg font-medium empty:w-24 empty:h-5 empty:rounded-full empty:animate-pulse empty:bg-gray-300 group-hover:text-red-500 ">
-              {person}
-            </span>
-            <div className="relative flex items-center justify-center text-white bg-red-500 rounded-full size-6">
-              <span className="z-10">{index}</span>
-              <div className="absolute bg-red-500 rounded-full size-6 animate-ping" />
-            </div>
-          </div>
-        ))}
-        <div className="flex flex-col group">
-          <input
-            className="w-full bg-gray-100"
-            placeholder="Write your email"
-          />
-          <span className="hidden group-focus-within:block">
-            Make sure it is a valid email
-          </span>
-          <button>submit</button>
+    <div className="flex flex-col items-center justify-between min-h-screen p-6">
+      <div className="my-auto flex flex-col items-center gap-2 *:font-medium">
+        <span className="text-9xl">🥕</span>
+        <h1 className="text-4xl ">당근</h1>
+        <h2 className="text-2xl">당근 마겟에 어서오세요!</h2>
+      </div>
+      <div className="flex flex-col items-center gap-3 w-full">
+        <Link
+          href="/create-account"
+          className="w-full bg-orange-500 text-white text-lg font-medium py-2.5 rounded-md text-center hover:bg-orange-400 transition-colors"
+        >
+          시작하기
+        </Link>
+        <div className="flex gap-2">
+          <span>이미 계정이 있나요?</span>
+          <Link href="/login" className="hover:underline">
+            로그인
+          </Link>
         </div>
       </div>
-    </main>
+    </div>
   );
 }
