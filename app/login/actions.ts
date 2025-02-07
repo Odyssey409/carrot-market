65c5c7d2b7aa9d5074e1b97725c2859d24cf5b72
Odyssey <77610388+Odyssey409@users.noreply.github.com>
2025-02-08 00:20:17 +0900

    Update actions.ts

--- app/login/actions.ts
+++ app/login/actions.ts
@@ -1,7 +1,7 @@
 "use server";
 
 export const handleForm = async (prevState: any, formData: FormData) => {
-  console.log(prevState);
+  console.log(prevState, formData);
   await new Promise((resolve) => setTimeout(resolve, 5000));
   return {
     errors: ["Invalid email or password", "Please try again"],
