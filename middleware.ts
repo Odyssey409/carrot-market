c1a1b3ffc9d887de4fb8ab5fa4c1fe8f23c2a79d
Odyssey <77610388+Odyssey409@users.noreply.github.com>
2025-03-31 16:10:28 +0900

    #8.10 Matcher-1
    실습하고 직후 지우기 전

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,19 @@
 import { NextRequest, NextResponse } from "next/server";
-import { getSession } from "@/lib/session";
 
 export async function middleware(request: NextRequest) {
-  const session = await getSession();
-  console.log(session);
-  if (request.nextUrl.pathname === "/profile") {
+  const pathname = request.nextUrl.pathname;
+  if (pathname === "/") {
+    const response = NextResponse.next();
+    response.cookies.set("middleware-cookie", "hello");
+    return response;
+  }
+
+  if (pathname === "/profile") {
     return NextResponse.redirect(new URL("/", request.url));
   }
 }
+
+export const config = {
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+  //정규표현식으로 해당 경로는 미들웨어를 거치지 않게 설정
+};
