4f849f35fd61c039e7ea746a4eb9dd78518c6a74
Odyssey <77610388+Odyssey409@users.noreply.github.com>
2025-04-01 00:45:56 +0900

    #8.13 Recap

--- middleware.ts
+++ middleware.ts
@@ -28,6 +28,6 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+  matcher: ["/((?!_next/static|_next/image|favicon.ico).*)"],
   //정규표현식으로 해당 경로는 미들웨어를 거치지 않게 설정
 };
