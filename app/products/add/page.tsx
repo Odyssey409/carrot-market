869ee6fb823751d524d58291b7fe3670bafbcbe8
Odyssey <odyssey409@naver.com>
2025-07-17 01:36:05 +0900

    #11.1 Form Action

--- app/products/add/page.tsx
+++ app/products/add/page.tsx
@@ -3,19 +3,55 @@ import Button from "@/components/button";
 import Input from "@/components/input";
 import { PhotoIcon } from "@heroicons/react/24/solid";
 import { useState } from "react";
+import { uploadProduct } from "./actions";
+import { z } from "zod";
+
+const fileSchema = z.object({
+  type: z.string().refine((value) => value.includes("image"), {
+    message: "이미지 파일만 업로드 가능합니다.",
+  }),
+  size: z.number().max(1024 * 1024 * 4, {
+    message: "이미지 파일은 4MB 이하로 업로드 가능합니다.",
+  }),
+});
 
 export default function AddProduct() {
   const [preview, setPreview] = useState("");
-  const onImageChange = () => {};
+  const onImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const {
+      target: { files },
+    } = event;
+    if (!files) return;
+    const file = files[0];
+    const result = fileSchema.safeParse(file);
+    if (!result.success) {
+      alert(
+        result.error.flatten().fieldErrors.type ||
+          result.error.flatten().fieldErrors.size
+      );
+      return;
+    }
+    const previewURL = URL.createObjectURL(file);
+    setPreview(previewURL);
+  };
   return (
     <div>
-      <form className="p-5 flex flex-col gap-5">
+      <form className="p-5 flex flex-col gap-5" action={uploadProduct}>
         <label
           htmlFor="photo"
-          className="border-2 aspect-square flex items-center justify-center flex-col text-neutral-300 border-neutral-300 rounded-md border-dashed cursor-pointer"
+          className="border-2 aspect-square flex items-center justify-center flex-col text-neutral-300 border-neutral-300 rounded-md border-dashed cursor-pointer bg-cover bg-center"
+          style={{
+            backgroundImage: `url(${preview})`,
+          }}
         >
-          <PhotoIcon className="w-20" />
-          <div className="text-neutral-400 text-sm">사진을 추가해주세요.</div>
+          {preview === "" ? (
+            <>
+              <PhotoIcon className="w-20" />
+              <div className="text-neutral-400 text-sm">
+                사진을 추가해주세요.
+              </div>
+            </>
+          ) : null}
         </label>
         <input
           onChange={onImageChange}
