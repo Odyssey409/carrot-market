bc8cd675d48547d215cb650ac44600042c9a6322
Odyssey <odyssey409@naver.com>
2025-07-20 00:16:54 +0900

    #11.2 Product Upload

--- app/products/add/actions.ts
+++ app/products/add/actions.ts
@@ -1,11 +1,56 @@
 "use server";
 
-export async function uploadProduct(formData: FormData) {
+import { z } from "zod";
+import fs from "fs/promises";
+import db from "@/lib/db";
+import { getSession } from "@/lib/session";
+import { redirect } from "next/navigation";
+
+const productSchema = z.object({
+  title: z.string({ required_error: "제목은 필수 입력 항목입니다." }),
+  description: z.string({ required_error: "설명은 필수 입력 항목입니다." }),
+  photo: z.string({ required_error: "사진은 필수 입력 항목입니다." }),
+  price: z.coerce.number({ required_error: "가격은 필수 입력 항목입니다." }),
+});
+
+export async function uploadProduct(_: any, formData: FormData) {
   const data = {
     title: formData.get("title"),
     price: formData.get("price"),
     description: formData.get("description"),
     photo: formData.get("photo"),
   };
-  console.log(data);
+  if (data.photo instanceof File) {
+    const photoData = await data.photo.arrayBuffer();
+    await fs.appendFile(`./public/${data.photo.name}`, Buffer.from(photoData));
+    // 유저가 업로드한 파일을 파일 시스템에 저장하는 방식의 임시 방편임. 이렇게 하면 좋지 않음. 개션 예정임.
+    data.photo = `/${data.photo.name}`;
+  }
+  const result = productSchema.safeParse(data);
+  if (!result.success) {
+    return {
+      error: result.error.flatten(),
+    };
+  } else {
+    const session = await getSession();
+    if (session.userId) {
+      const product = await db.product.create({
+        data: {
+          title: result.data.title,
+          description: result.data.description,
+          photo: result.data.photo,
+          price: result.data.price,
+          user: {
+            connect: {
+              id: session.userId,
+            },
+          },
+        },
+        select: {
+          id: true,
+        },
+      });
+      redirect(`/products/${product.id}`);
+    }
+  }
 }
--- app/products/add/page.tsx
+++ app/products/add/page.tsx
@@ -5,6 +5,7 @@ import { PhotoIcon } from "@heroicons/react/24/solid";
 import { useState } from "react";
 import { uploadProduct } from "./actions";
 import { z } from "zod";
+import { useFormState } from "react-dom";
 
 const fileSchema = z.object({
   type: z.string().refine((value) => value.includes("image"), {
@@ -34,9 +35,10 @@ export default function AddProduct() {
     const previewURL = URL.createObjectURL(file);
     setPreview(previewURL);
   };
+  const [state, action] = useFormState(uploadProduct, null);
   return (
     <div>
-      <form className="p-5 flex flex-col gap-5" action={uploadProduct}>
+      <form className="p-5 flex flex-col gap-5" action={action}>
         <label
           htmlFor="photo"
           className="border-2 aspect-square flex items-center justify-center flex-col text-neutral-300 border-neutral-300 rounded-md border-dashed cursor-pointer bg-cover bg-center"
@@ -49,6 +51,7 @@ export default function AddProduct() {
               <PhotoIcon className="w-20" />
               <div className="text-neutral-400 text-sm">
                 사진을 추가해주세요.
+                {state?.error?.fieldErrors.photo}
               </div>
             </>
           ) : null}
@@ -61,13 +64,26 @@ export default function AddProduct() {
           accept="image/*"
           className="hidden"
         />
-        <Input name="title" required placeholder="제목" type="text" />
-        <Input name="price" type="number" required placeholder="가격" />
+        <Input
+          name="title"
+          required
+          placeholder="제목"
+          type="text"
+          errors={state?.error?.fieldErrors.title}
+        />
+        <Input
+          name="price"
+          type="number"
+          required
+          placeholder="가격"
+          errors={state?.error?.fieldErrors.price}
+        />
         <Input
           name="description"
           type="text"
           required
           placeholder="자세한 설명"
+          errors={state?.error?.fieldErrors.description}
         />
         <Button text="작성 완료" />
       </form>
