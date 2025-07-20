bde6bed8d9f2620b213bfab5516c7ac7f6bb34e1
Odyssey <odyssey409@naver.com>
2025-07-21 00:13:28 +0900

    주석 수정

--- app/products/add/actions.ts
+++ app/products/add/actions.ts
@@ -23,7 +23,7 @@ export async function uploadProduct(_: any, formData: FormData) {
   if (data.photo instanceof File) {
     const photoData = await data.photo.arrayBuffer();
     await fs.appendFile(`./public/${data.photo.name}`, Buffer.from(photoData));
-    // 유저가 업로드한 파일을 파일 시스템에 저장하는 방식의 임시 방편임. 이렇게 하면 좋지 않음. 개션 예정임.
+    // 유저가 업로드한 파일을 파일 시스템에 저장하는 방식의 임시 방편임. 이렇게 하면 좋지 않음. 개선 예정임. 아마 클라우드 스토리지 사용하거나 데이터베이스에 저장하는 방식으로 변경해야 할 듯. 보통 유료임.
     data.photo = `/${data.photo.name}`;
   }
   const result = productSchema.safeParse(data);
